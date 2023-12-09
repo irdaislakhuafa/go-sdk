5ad9bf32ac1162389439248e0c15e2c1e10f77b4
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-12-10 03:33:54 +0700

    added function to compile http status text to specific language

--- language/language.go
+++ language/language.go
@@ -1,8 +1,21 @@
 package language
 
+type Language string
+
 const (
-	English    = "EN"
-	Indonesian = "ID"
-	Japanese   = "JP"
-	Duetsch    = "DE"
+	English    = Language("EN")
+	Indonesian = Language("ID")
+	Japanese   = Language("JP")
+	Duetsch    = Language("DE")
 )
+
+var (
+	httpStatusLanguages = map[Language](map[int]string){
+		English:    statusTextEN,
+		Indonesian: statusTextID,
+	}
+)
+
+func HTTPStatusText(lang Language, code int) string {
+	return httpStatusLanguages[lang][code]
+}
