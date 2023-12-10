1640923458640d9fe89e5fc97760cae3e44a3a66
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2023-12-11 01:10:52 +0700

    added `log` package to make it easier to tracking log with multi-level log in golang (#11)
    * added unitinalize method for log, and added init method for initialize log
    
    * pending development at this branch
    
    * added method to get caller at log
    
    * pending, waiting package `appcontext`
    
    * added function to get standard (at least in this framework) fields from context
    
    * implement all log methods

---
+++ log/log.go
@@ -0,0 +1,125 @@
+package log
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/irdaislakhuafa/go-sdk/appcontext"
+	"github.com/irdaislakhuafa/go-sdk/errors"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var once sync.Once = sync.Once{}
+
+type Interface interface {
+	// TODO: added method Debugf
+	Trace(ctx context.Context, obj interface{})
+	Debug(ctx context.Context, obj interface{})
+	Info(ctx context.Context, obj interface{})
+	Warn(ctx context.Context, obj interface{})
+	Error(ctx context.Context, obj interface{})
+	Fatal(ctx context.Context, obj interface{})
+}
+
+type Config struct {
+	Level string
+}
+
+type logger struct {
+	log zerolog.Logger
+}
+
+const (
+	skipFrameCount = 3 // NOTE: temporary 3 for now
+)
+
+func Init(cfg Config) Interface {
+	var zerologger zerolog.Logger
+	once.Do(func() {
+		level, err := zerolog.ParseLevel(cfg.Level)
+		if err != nil {
+			log.Fatal().Msg(fmt.Sprintf("failed to parse log level from config with err: %v", err))
+		}
+
+		zerologger = zerolog.New(os.Stdout).
+			With().
+			Timestamp().
+			CallerWithSkipFrameCount(skipFrameCount).
+			Logger().
+			Level(level)
+	})
+
+	return &logger{log: zerologger}
+}
+
+func (l *logger) Trace(ctx context.Context, obj interface{}) {
+	l.log.Trace().
+		Fields(getContextFields(ctx)).
+		Msg(fmt.Sprint(GetCaller(ctx)))
+}
+
+func (l *logger) Debug(ctx context.Context, obj interface{}) {
+	l.log.Debug().
+		Fields(getContextFields(ctx)).
+		Msg(fmt.Sprint(GetCaller(ctx)))
+}
+
+func (l *logger) Info(ctx context.Context, obj interface{}) {
+	l.log.Info().
+		Fields(getContextFields(ctx)).
+		Msg(fmt.Sprint(GetCaller(ctx)))
+}
+
+func (l *logger) Warn(ctx context.Context, obj interface{}) {
+	l.log.Warn().
+		Fields(getContextFields(ctx)).
+		Msg(fmt.Sprint(GetCaller(ctx)))
+}
+
+func (l *logger) Error(ctx context.Context, obj interface{}) {
+	l.log.Error().
+		Fields(getContextFields(ctx)).
+		Msg(fmt.Sprint(GetCaller(ctx)))
+}
+
+func (l *logger) Fatal(ctx context.Context, obj interface{}) {
+	l.log.Fatal().
+		Fields(getContextFields(ctx)).
+		Msg(fmt.Sprint(GetCaller(ctx)))
+}
+
+func GetCaller(value any) any {
+	switch tErr := value.(type) {
+	case error:
+		file, line, message, err := errors.GetCaller(tErr)
+		if err != nil {
+			return fmt.Sprintf("%s:%#v --- %s", file, line, message)
+		}
+	case string:
+		return tErr
+	default:
+		return fmt.Sprintf("#%v", tErr)
+	}
+
+	return value
+}
+
+func getContextFields(ctx context.Context) map[string]any {
+	reqStartTime := appcontext.GetRequestStartTime(ctx)
+	timeElapsed := "0ms"
+
+	if !reqStartTime.IsZero() {
+		timeElapsed = fmt.Sprintf("%dms", uint64(time.Since(reqStartTime)/time.Millisecond))
+	}
+
+	return map[string]any{
+		"request_id":      appcontext.GetRequestID(ctx),
+		"user_agent":      appcontext.GetUserAgent(ctx),
+		"service_version": appcontext.GetServiceVersion(ctx),
+		"time_elapsed":    timeElapsed,
+	}
+}
