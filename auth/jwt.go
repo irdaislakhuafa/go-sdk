94d9c1b1ac2fb279124d024e7141e8c29c02f93c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-01-15 12:22:44 +0700

    added utility method to generate/validate/extract jwt token

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,4 +1,79 @@
 package auth
 
-type JWTInterface struct {
+import (
+	"context"
+	"reflect"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/irdaislakhuafa/go-sdk/codes"
+	"github.com/irdaislakhuafa/go-sdk/errors"
+)
+
+type JWTInterface[C jwt.Claims] interface {
+	Generate(ctx context.Context) (string, error)
+	Validate(ctx context.Context, tokenString string) (*jwt.Token, error)
+	ExtractClaims(ctx context.Context, jwtToken *jwt.Token) (C, error)
+	WithSigningMethod(signingMethod jwt.SigningMethod) JWTInterface[C]
+}
+
+type jwtimpl[C jwt.Claims] struct {
+	secretKey     []byte
+	claims        C
+	signingMethod jwt.SigningMethod
+}
+
+func Init[C jwt.Claims](secretKey []byte, claims C) JWTInterface[C] {
+	j := jwtimpl[C]{
+		secretKey:     secretKey,
+		claims:        claims,
+		signingMethod: jwt.SigningMethodHS256,
+	}
+	return &j
+}
+
+func (j *jwtimpl[C]) Generate(ctx context.Context) (string, error) {
+	jwtToken := jwt.NewWithClaims(j.signingMethod, j.claims)
+	jwtString, err := jwtToken.SignedString(j.secretKey)
+	if err != nil {
+		return "", errors.NewWithCode(codes.CodeJWTSignedStringError, "cannot signed string, %v", err.Error())
+	}
+
+	return jwtString, nil
+}
+
+func (j *jwtimpl[C]) Validate(ctx context.Context, tokenString string) (*jwt.Token, error) {
+	kind := reflect.TypeOf(j.claims).Kind()
+
+	switch kind {
+	case reflect.Pointer:
+		keyFunc := func(jwtToken *jwt.Token) (any, error) {
+			if _, isOk := jwtToken.Method.(*jwt.SigningMethodHMAC); !isOk {
+				return nil, errors.NewWithCode(codes.CodeJWTInvalidMethod, "invalid token method algoritm")
+			}
+			return j.secretKey, nil
+		}
+
+		jwtToken, err := jwt.ParseWithClaims(tokenString, j.claims, keyFunc)
+		if err != nil {
+			return nil, errors.NewWithCode(codes.CodeJWTParseWithClaimsError, "cannot parse token with claims, %v", err)
+		}
+
+		return jwtToken, nil
+	default:
+		return nil, errors.NewWithCode(codes.CodeJWTInvalidClaimsType, "claims type must be a pointer but got %v", kind.String())
+	}
+}
+
+func (j *jwtimpl[C]) ExtractClaims(ctx context.Context, jwtToken *jwt.Token) (C, error) {
+	claims, isOk := jwtToken.Claims.(C)
+	if !isOk {
+		return j.claims, errors.NewWithCode(codes.CodeJWTInvalidClaimsType, "claims type is not equals")
+	}
+
+	return claims, nil
+}
+
+func (j *jwtimpl[C]) WithSigningMethod(signingMethod jwt.SigningMethod) JWTInterface[C] {
+	j.signingMethod = signingMethod
+	return j
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,17 @@ module github.com/irdaislakhuafa/go-sdk
 
 go 1.20
 
-require github.com/rs/zerolog v1.31.0
+require (
+	github.com/go-mail/gomail v2.3.1+incompatible
+	github.com/rs/zerolog v1.31.0
+	golang.org/x/crypto v0.17.0
+)
 
 require (
-	github.com/go-mail/gomail v2.3.1+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/mail.v2 v2.3.1 // indirect
 )
