8eb326a26b6f7394144a97c702200195699f09c4
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2024-01-22 00:09:05 +0700

    added more codes for go-identiface and codes for str template (#26)

--- strformat/strformat.go
+++ strformat/strformat.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"regexp"
 	"text/template"
+
+	"github.com/irdaislakhuafa/go-sdk/codes"
+	"github.com/irdaislakhuafa/go-sdk/errors"
 )
 
 // Check string value, will return `true` if string value is only number and return `false` if string value not only contains number
@@ -19,12 +22,12 @@ func IsOnlyNumber(s string) bool {
 func Tmpl(tmplFormat string, values any) (string, error) {
 	tmpl, err := template.New("").Parse(tmplFormat)
 	if err != nil {
-		return "", err
+		return "", errors.NewWithCode(codes.CodeStrTemplateInvalidFormat, "cannot parse str format, %v", err)
 	}
 
 	buff := bytes.Buffer{}
 	if err := tmpl.Execute(&buff, values); err != nil {
-		return "", err
+		return "", errors.NewWithCode(codes.CodeStrTemplateExecuteErr, "cannot execute template, %v", err)
 	}
 
 	return buff.String(), nil
