e9c17eae0b63c04e0231b449918c30a94b685cbe
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-02-07 03:06:48 +0700

    call method Error() for message if cause error is not instance of pointer stacktrace

--- errors/errors.go
+++ errors/errors.go
@@ -99,7 +99,7 @@ func GetCaller(cause error) (file string, line int, message string, err error) {
 	if st, isOk := cause.(*stacktrace); isOk {
 		return st.file, st.line, st.message, nil
 	} else {
-		return "", 0, "", create(nil, codes.NoCode, "failed to cast error to stacktrace")
+		return "", 0, cause.Error(), create(nil, codes.NoCode, "failed to cast error to stacktrace")
 	}
 }
 
