a37392610b3d5e598fa0c8e4feddd715ac832583
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-02-21 18:26:43 +0700

    added method to convert ptr value to safe value

--- convert/ptr.go
+++ convert/ptr.go
@@ -1,6 +1,29 @@
 package convert
 
+import "reflect"
+
 // Convert value to pointer of data type
 func ToPointer[T any](value T) *T {
 	return &value
+}
+
+func ToSafeValue[T any](value any) T {
+	rv := reflect.ValueOf(value)
+	for rv.Kind() == reflect.Pointer {
+		if !rv.IsNil() {
+			rv = rv.Elem()
+		} else {
+			break
+		}
+	}
+
+	if !rv.IsValid() {
+		return ToSafeValue[T](new(T))
+	}
+
+	safeValue, isOk := rv.Interface().(T)
+	if !isOk {
+		return ToSafeValue[T](new(T))
+	}
+	return safeValue
 }
