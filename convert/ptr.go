bd6ab00b872b6efc1192d3480b50042025c73641
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2024-02-22 11:32:05 +0700

    method to convert unsafe value to safe value (#36)
    * added method to convert ptr value to safe value
    
    * added documentation for method `ToSafeValue()`

--- convert/ptr.go
+++ convert/ptr.go
@@ -1,6 +1,30 @@
 package convert
 
+import "reflect"
+
 // Convert value to pointer of data type
 func ToPointer[T any](value T) *T {
 	return &value
+}
+
+// Convert any value to `T` data type and if value is not valid or `nil`, will use default value of `T` (null/nil safety) as return value
+func ToSafeValue[T any](value any) T {
+	rv := reflect.ValueOf(value)
+	for rv.Kind() == reflect.Pointer {
+		if !rv.IsNil() {
+			rv = rv.Elem()
+		} else {
+			break
+		}
+	}
+
+	if !rv.IsValid() {
+		return ToSafeValue[T](new(T))
+	}
+
+	safeValue, isOk := rv.Interface().(T)
+	if !isOk {
+		return ToSafeValue[T](new(T))
+	}
+	return safeValue
 }
--- convert/ptr_test.go
+++ convert/ptr_test.go
@@ -29,3 +29,93 @@ func Test_ToPointer(t *testing.T) {
 		})
 	}
 }
+
+func Test_ToSafeValue(t *testing.T) {
+	type DataType int
+	const (
+		INT = DataType(iota + 1)
+		FLOAT
+		STRING
+		STRUCT
+	)
+
+	type (
+		exampleStruct struct {
+			Anything string
+		}
+
+		params struct {
+			dataType DataType
+			value    any
+		}
+
+		want struct {
+			value any
+		}
+
+		test struct {
+			name   string
+			params params
+			want   want
+		}
+	)
+	tests := []test{
+		{
+			name:   "int nil ptr",
+			params: params{dataType: INT, value: nil},
+			want:   want{value: 0},
+		},
+		{
+			name:   "int ptr value 10",
+			params: params{dataType: INT, value: ToPointer(10)},
+			want:   want{value: 10},
+		},
+		{
+			name:   "string ptr",
+			params: params{dataType: STRING, value: nil},
+			want:   want{value: ""},
+		},
+		{
+			name:   "string ptr value 'str'",
+			params: params{dataType: STRING, value: ToPointer("str")},
+			want:   want{value: "str"},
+		},
+		{
+			name:   "struct ptr",
+			params: params{dataType: STRUCT, value: nil},
+			want:   want{value: exampleStruct{}},
+		},
+		{
+			name:   "struct ptr value",
+			params: params{dataType: STRUCT, value: ToPointer(exampleStruct{Anything: "haha"})},
+			want:   want{value: exampleStruct{Anything: "haha"}},
+		},
+		{
+			name:   "struct ptr value empty str",
+			params: params{dataType: STRUCT, value: ToPointer("")},
+			want:   want{value: exampleStruct{}},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var result any
+			switch tt.params.dataType {
+			case INT:
+				result = ToSafeValue[int](tt.params.value)
+			case STRING:
+				result = ToSafeValue[string](tt.params.value)
+			case FLOAT:
+				result = ToSafeValue[float64](tt.params.value)
+			case STRUCT:
+				result = ToSafeValue[exampleStruct](tt.params.value)
+			default:
+				t.Fatalf("data type not supported!")
+			}
+
+			if result != tt.want.value {
+				t.Fatalf("want result is '%v' but got '%v'", tt.want.value, result)
+			}
+		})
+	}
+}
