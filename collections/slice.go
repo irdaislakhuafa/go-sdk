a8dcd88a7c931d57c0e005378d8a4d92d406d224
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-04-26 18:31:56 +0700

    feat(collections): check if elements of 2 slices are equal with ignored order
    Adds a new function `IsElementsEquals` to the `collections` package that checks if the elements of two slices are equal, ignoring the order of the elements. This can be useful for comparing two slices that contain the same elements but in a different order.

--- collections/slice.go
+++ collections/slice.go
@@ -2,9 +2,14 @@ package collections
 
 import "slices"
 
+// Will check is elements of 2 slice is equals with ignored order
 func IsElementsEquals[T comparable](a, b []T) bool {
-	for _, v := range a {
-		if !slices.Contains(b, v) {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for _, v := range b {
+		if !slices.Contains(a, v) {
 			return false
 		}
 	}
