8f5e0144487e956ffa08d6decb4e30c0cfa0dee0
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2024-05-14 18:34:47 +0700

    refactor: rename mutex function name (#56)
    * refactor: rename mutex function name
    
    Rename WithMt to WithLocker to generalize locker implementation
    
    * feat(concurrency): introduce custom sync.Locker
    
    Changes the WithLocker method to use sync.Locker instead of sync.Mutex.
    This allows users to use their own custom Locker implementations, giving them more flexibility and control over locking behavior.
    
    * fix: remove `[]int` string from logging
    
    Replaces `[]int` strings with their values in logging.
    
    * feat(concurrency): async query method to run list functions with goroutine
    
    This commit introduces a new method to run list functions with goroutine. The list method will be cleared after calling this method.
    
    This change improves the performance and scalability of the concurrency package.

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -10,22 +10,22 @@ type Interface interface {
 	// Use your custom sync.WaitGroup
 	WithWg(wg *sync.WaitGroup) Interface
 
-	// Use your custom sync.Mutex
-	WithMt(mt *sync.Mutex) Interface
+	// Use your custom sync.Locker
+	WithLocker(lc sync.Locker) Interface
 
 	// Set maximum worker here. Default is 1
 	WithMaxWorker(maxWorker int64) Interface
 
-	// Run the list functions with goroutine
+	// Run the list functions with goroutine. The list method will be cleared after calling this method.
 	Do(ctx context.Context) error
 
 	// Added function that will be run async at goroutine. This method already call c.Done() after process is complete
 	AddFunc(fn func(ctx context.Context, c Interface))
 
-	// Lock block of code. This like mt.Lock()
+	// Lock block of code. This like (sync.Mutex{}).Lock()
 	Lock()
 
-	// Unlock block of code. This like mt.Unlock()
+	// Unlock block of code. This like (sync.Mutex{}).Unlock()
 	Unlock()
 
 	// To flag wait group if proccess is done. This like wg.Done()
@@ -40,7 +40,7 @@ type Interface interface {
 
 type concurrency struct {
 	wg        *sync.WaitGroup
-	mt        *sync.Mutex
+	lc        sync.Locker
 	maxWorker int64
 	listErr   []error
 	listFunc  []func(ctx context.Context, c Interface)
@@ -50,7 +50,7 @@ type concurrency struct {
 func NewConcurrency() Interface {
 	result := concurrency{
 		wg:        &sync.WaitGroup{},
-		mt:        &sync.Mutex{},
+		lc:        &sync.Mutex{},
 		maxWorker: 1,
 	}
 	return &result
@@ -61,8 +61,8 @@ func (c *concurrency) WithWg(wg *sync.WaitGroup) Interface {
 	return c
 }
 
-func (c *concurrency) WithMt(mt *sync.Mutex) Interface {
-	c.mt = mt
+func (c *concurrency) WithLocker(lc sync.Locker) Interface {
+	c.lc = lc
 	return c
 }
 
@@ -100,11 +100,11 @@ func (c *concurrency) AddFunc(fn func(ctx context.Context, c Interface)) {
 }
 
 func (c *concurrency) Lock() {
-	c.mt.Lock()
+	c.lc.Lock()
 }
 
 func (c *concurrency) Unlock() {
-	c.mt.Unlock()
+	c.lc.Unlock()
 }
 
 func (c *concurrency) Done() {
