5ee77b94ccb88583e4da5e8820dba1e8132f45a2
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2024-05-14 10:17:37 +0700

    test: add concurrency test (#54)
    * test: add concurrency test
    
    Add concurrency test cases for concurrency.go.
    
    This test case covers the following scenarios:
    - Concurrency get data
    
    * refactor: remove unused main package
    
    The main package was no longer being used and was causing unnecessary clutter in the codebase. This commit removes the main package to improve code organization and maintainability.

---
+++ concurrency/concurrency_test.go
@@ -0,0 +1,78 @@
+package concurrency
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/irdaislakhuafa/go-sdk/codes"
+	"github.com/irdaislakhuafa/go-sdk/collections"
+	"github.com/irdaislakhuafa/go-sdk/errors"
+)
+
+func Test_Concurrency(t *testing.T) {
+	type (
+		args struct {
+			worker  int
+			process int
+		}
+
+		want struct {
+			intValues []int
+			errCode   codes.Code
+		}
+
+		test struct {
+			name string
+			args args
+			want want
+		}
+	)
+
+	tests := []test{
+		{
+			name: "concurrency get data",
+			args: args{worker: 2, process: 10},
+			want: want{intValues: []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, errCode: codes.NoCode},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			intValues := []int{}
+			ctx := context.Background()
+			c := NewConcurrency().WithMaxWorker(int64(tt.args.worker))
+			for index := range tt.want.intValues[:len(tt.want.intValues)/2] {
+				i := tt.want.intValues[index]
+				c.AddFunc(func(ctx context.Context, c Interface) {
+					time.Sleep(time.Second * 3)
+					c.Lock()
+					intValues = append(intValues, i)
+					t.Logf("%#v", intValues)
+					c.Unlock()
+				})
+			}
+			if err := c.Do(ctx); err != nil {
+				t.Fatalf("want err code '%v' but got '%v'", tt.want.errCode, errors.GetCode(err))
+			}
+
+			for index := range tt.want.intValues[len(tt.want.intValues)/2:] {
+				i := tt.want.intValues[len(tt.want.intValues)/2+index]
+				c.AddFunc(func(ctx context.Context, c Interface) {
+					time.Sleep(time.Second * 3)
+					c.Lock()
+					intValues = append(intValues, i)
+					t.Logf("%#v", intValues)
+					c.Unlock()
+				})
+			}
+
+			if err := c.Do(ctx); err != nil {
+				t.Fatalf("want err code '%v' but got '%v'", tt.want.errCode, errors.GetCode(err))
+			}
+
+			if !collections.IsElementsEquals(tt.want.intValues, intValues) {
+				t.Fatalf("want result values is '%#v' but got '%#v'", tt.want.intValues, intValues)
+			}
+		})
+	}
+}
