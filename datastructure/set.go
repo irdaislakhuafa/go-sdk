63aec17182e22faaecc2ccd15d9fe53239d6749c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2024-05-15 13:31:25 +0700

    feat(set): Added new methods and improved existing methods
    Added the following new methods:
    
    - IsExists: Checks if an element exists in the set.
    - Del: Deletes an element from the set.
    - Convert: Converts the set to a slice.
    - Clear: Clears all elements from the set.
    - IsEmpty: Checks if the set is empty.
    - Size: Gets the size of the set.
    - DelIf: Deletes elements from the set based on a condition.

--- datastructure/set.go
+++ datastructure/set.go
@@ -1,28 +1,43 @@
 package datastructure
 
 type Set[E comparable] struct {
-	Values map[E]bool
+	values map[E]bool
 }
 
 type SetInterface[E comparable] interface {
+	// Added list element to set.
 	Add(values ...E)
+
+	// To check is element is exists on set.
 	IsExists(value E) bool
+
+	// Delete list element of set.
 	Del(values ...E)
+
+	// Convert set to slice. Maybe you need it for interation.
 	Slice() []E
+
+	// Clear list element from set.
 	Clear()
+
+	// Check is set is empty. Return true is empty and false is not empty.
 	IsEmpty() bool
+
+	// Get size of element on set.
 	Size() int
+
+	// Delete element from set if condition is matched.
 	DelIf(condition func(value E) bool) SetInterface[E]
 }
 
 // Set data type is like `slice` or `array` but without duplicate element and set element is unordered element
 func NewSet[E comparable](values ...E) SetInterface[E] {
 	set := Set[E]{
-		Values: map[E]bool{},
+		values: map[E]bool{},
 	}
 
 	for _, v := range values {
-		set.Values[v] = true
+		set.values[v] = true
 	}
 
 	return &set
@@ -30,43 +45,43 @@ func NewSet[E comparable](values ...E) SetInterface[E] {
 
 func (s *Set[E]) Add(values ...E) {
 	for _, v := range values {
-		s.Values[v] = true
+		s.values[v] = true
 	}
 }
 
 func (s *Set[E]) IsExists(value E) bool {
-	_, isExists := s.Values[value]
+	_, isExists := s.values[value]
 	return isExists
 }
 
 func (s *Set[E]) Del(values ...E) {
 	for _, v := range values {
-		delete(s.Values, v)
+		delete(s.values, v)
 	}
 }
 
 func (s *Set[E]) Slice() []E {
 	results := []E{}
-	for v := range s.Values {
+	for v := range s.values {
 		results = append(results, v)
 	}
 	return results
 }
 
 func (s *Set[E]) Clear() {
-	clear(s.Values)
+	s.values = nil
 }
 
 func (s *Set[E]) IsEmpty() bool {
-	return len(s.Values) == 0
+	return len(s.values) == 0
 }
 
 func (s *Set[E]) Size() int {
-	return len(s.Values)
+	return len(s.values)
 }
 
 func (s *Set[E]) DelIf(condition func(value E) bool) SetInterface[E] {
-	for v := range s.Values {
+	for v := range s.values {
 		if condition(v) {
 			s.Del(v)
 		}
