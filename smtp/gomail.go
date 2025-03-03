5786a1af5ca7c6136a45227e420a732bfa11d22d
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-03-03 15:22:23 +0700

    feat: improve smtp implementation, but breaking changes with previous version

--- smtp/gomail.go
+++ smtp/gomail.go
@@ -1,27 +1,47 @@
 package smtp
 
 import (
+	"context"
+
 	mail "github.com/go-mail/gomail"
 	"github.com/irdaislakhuafa/go-sdk/codes"
 	"github.com/irdaislakhuafa/go-sdk/errors"
+	"github.com/irdaislakhuafa/go-sdk/strformat"
 )
 
-type GoMailInterface interface {
-	DialAndSend(messages ...*mail.Message) error
-}
-
-type gomailImpl struct {
+type gomail struct {
 	dialer *mail.Dialer
 }
 
-func InitGoMail(cfg Config) GoMailInterface {
-	result := &gomailImpl{
+func InitGoMail(cfg Config) Interface {
+	result := &gomail{
 		dialer: mail.NewDialer(cfg.Host, int(cfg.Port), cfg.Username, cfg.Password),
 	}
 	return result
 }
 
-func (g *gomailImpl) DialAndSend(messages ...*mail.Message) error {
+func (g *gomail) Send(ctx context.Context, msgs ...Message) error {
+	messages := []*mail.Message{}
+	for _, msg := range msgs {
+		m := mail.NewMessage()
+		m.SetHeaders(map[string][]string{
+			"From":    msg.Headers.From,
+			"To":      msg.Headers.To,
+			"Subject": msg.Headers.Subject,
+		})
+		for hk, hv := range msg.Headers.Extras {
+			m.SetHeader(hk, hv...)
+		}
+
+		body, err := strformat.T(msg.Body.Template, msg.Body.Values)
+		if err != nil {
+			return errors.NewWithCode(codes.CodeStrTemplateExecuteErr, err.Error())
+		}
+
+		m.SetBody(msg.Body.ContentType, body)
+		messages = append(messages, m)
+	}
+
 	if err := g.dialer.DialAndSend(messages...); err != nil {
 		return errors.NewWithCode(codes.CodeSMTP, "failed to send messages, %v", err)
 	}
--- smtp/smtp.go
+++ smtp/smtp.go
@@ -1,8 +1,62 @@
 package smtp
 
-type Config struct {
-	Username string
-	Password string
-	Host     string
-	Port     int64
+import (
+	"context"
+
+	"github.com/irdaislakhuafa/go-sdk/codes"
+	"github.com/irdaislakhuafa/go-sdk/errors"
+)
+
+type (
+	Interface interface {
+		Send(ctx context.Context, msgs ...Message) error
+	}
+
+	MessageHeaders struct {
+		From    []string
+		To      []string
+		Subject []string
+		Extras  map[string][]string
+	}
+
+	MessageBody struct {
+		ContentType string
+		Template    string
+		Values      map[string]any
+	}
+
+	Message struct {
+		Headers MessageHeaders
+		Body    MessageBody
+	}
+
+	Provider string
+
+	Config struct {
+		Username string
+		Password string
+		Host     string
+		Port     int64
+		Provider Provider
+	}
+)
+
+const (
+	ProviderGoMail = Provider("gomail")
+)
+
+func (c *Config) Default() {
+	if c.Provider == "" {
+		c.Provider = ProviderGoMail
+	}
+}
+
+func Init(cfg Config) (Interface, error) {
+	cfg.Default()
+
+	if cfg.Provider == ProviderGoMail {
+		return InitGoMail(cfg), nil
+	} else {
+		return nil, errors.NewWithCode(codes.CodeNotImplemented, "smtp provider not supported!")
+	}
 }
