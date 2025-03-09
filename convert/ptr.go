1d64db17fa32c7a997db38acfc511b22dcf6936b
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-03-09 13:49:01 +0700

    ✨ feat: add methods to convert sql null to nil if invalid and to real value if valid

--- convert/ptr.go
+++ convert/ptr.go
@@ -1,6 +1,10 @@
 package convert
 
-import "reflect"
+import (
+	"database/sql"
+	"reflect"
+	"time"
+)
 
 // Convert value to pointer of data type
 func ToPointer[T any](value T) *T {
@@ -27,4 +31,60 @@ func ToSafeValue[T any](value any) T {
 		return ToSafeValue[T](new(T))
 	}
 	return safeValue
+}
+
+// Convert sql.NullTime to *time.Time is valid and to nil is invalid
+func SQLNullTimeToNil(value sql.NullTime) *time.Time {
+	if !value.Valid {
+		return nil
+	}
+	return &value.Time
+}
+
+// Convert sql.NullInt16 to *int16 is valid and to nil is invalid
+func SQLNullInt16ToNil(value sql.NullInt16) *int16 {
+	if !value.Valid {
+		return nil
+	}
+	return &value.Int16
+}
+
+// Convert sql.NullInt32 to *int32 is valid and to nil is invalid
+func SQLNullInt32ToNil(value sql.NullInt32) *int32 {
+	if !value.Valid {
+		return nil
+	}
+	return &value.Int32
+}
+
+// Convert sql.NullInt64 to *int64 is valid and to nil is invalid
+func SQLNullInt64ToNil(value sql.NullInt64) *int64 {
+	if !value.Valid {
+		return nil
+	}
+	return &value.Int64
+}
+
+// Convert sql.NullFloat64 to *float64 is valid and to nil is invalid
+func SQLNullFloat64ToNil(value sql.NullFloat64) *float64 {
+	if !value.Valid {
+		return nil
+	}
+	return &value.Float64
+}
+
+// Convert sql.NullString to *string is valid and to nil is invalid
+func SQLNullStringToNil(value sql.NullString) *string {
+	if !value.Valid {
+		return nil
+	}
+	return &value.String
+}
+
+// Convert sql.NullBool to *bool is valid and to nil is invalid
+func SQLNullBoolToNil(value sql.NullBool) *bool {
+	if !value.Valid {
+		return nil
+	}
+	return &value.Bool
 }
