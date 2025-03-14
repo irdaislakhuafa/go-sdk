daaec59793af96305b35cf0c9b1b92c0a55b8efb
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2025-03-15 05:18:41 +0700

    feat(collections): add map function for slices (#79)

---
+++ collections/map_test.go
@@ -0,0 +1,68 @@
+package collections
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func Test_Map(t *testing.T) {
+	type (
+		args struct {
+			values []int
+		}
+
+		want struct {
+			values []string
+			err    error
+		}
+
+		mode int
+		test struct {
+			name string
+			mode mode
+			args args
+			want want
+		}
+	)
+
+	const (
+		MODE_MAP = mode(iota)
+		MODE_MAP_WITH_ERR
+	)
+
+	tests := []test{
+		{
+			name: "map int to string success",
+			mode: MODE_MAP,
+			args: args{values: []int{1, 2, 3}},
+			want: want{values: []string{"1", "2", "3"}, err: nil},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			switch tt.mode {
+			case MODE_MAP:
+				res := Map(tt.args.values, func(value int) string {
+					return fmt.Sprint(value)
+				})
+
+				if strings.Join(res, ",") != strings.Join(tt.want.values, ",") {
+					t.Fatalf("want is %+v but got %+v", tt.want.values, res)
+				}
+			case MODE_MAP_WITH_ERR:
+				res, err := MapWithErr(tt.args.values, func(value int) (string, error) {
+					return fmt.Sprint(value), nil
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+
+				if strings.Join(res, ",") != strings.Join(tt.want.values, ",") {
+					t.Fatalf("want is %+v but got %+v", tt.want.values, res)
+				}
+			}
+		})
+	}
+}
